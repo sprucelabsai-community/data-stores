5f924b6a05224e9c2573f278dd924fbc8824851a
Taylor Romero <me@taylorrome.ro>
2022-12-27 07:34:57 -0700

    patch: error handling capturing (not sure next steps to recover connection)

---
+++ src/__tests__/behavioral/HandlingMongoErrors.test.ts
@@ -0,0 +1,25 @@
+import { test } from '@sprucelabs/test-utils'
+import MongoDatabase, { MONGO_TEST_URI } from '../../databases/MongoDatabase'
+import AbstractDatabaseTest from '../../tests/AbstractDatabaseTest'
+
+export default class HandlingMongoErrorsTest extends AbstractDatabaseTest {
+	@test()
+	protected static async canCreateHandlingMongoErrors() {
+		const database = new ThrowingMongDatabase(MONGO_TEST_URI, {
+			dbName: 'testing-lost-connections',
+		})
+
+		await database.connect()
+
+		database.throwOnTopology(new Error('not great yo!'))
+
+		await database.close()
+	}
+}
+
+class ThrowingMongDatabase extends MongoDatabase {
+	public throwOnTopology(err: Error) {
+		//@ts-ignore
+		this.mongo.topology.emit('error', err)
+	}
+}
