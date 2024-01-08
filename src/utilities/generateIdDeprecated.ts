0c46ab27e0b8bfb6b2b2c93f1944b8d218a7b5b7
Taylor Romero <me@taylorrome.ro>
2024-01-08 14:28:52 -0700

    patch: remove deprecation notic

--- src/utilities/generateIdDeprecated.ts
+++
@@ -1,7 +0,0 @@
-import generateId from './generateId'
-/**
- * @deprecated change generateId to use @sprucelabs/spruce-test-utils generateId
- */
-const generateIdDeprecated = generateId
-
-export default generateIdDeprecated
