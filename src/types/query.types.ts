796bf771ef6480c0f2a9615489cbe32de4ba88cd
Taylor Romero <me@taylorrome.ro>
2024-12-01 17:27:13 -0700

    patch: type changes

--- src/types/query.types.ts
+++ src/types/query.types.ts
@@ -1,50 +1,14 @@
+import { PathsWithDotNotation } from '@sprucelabs/schema'
 import { QuerySelector, RootQuerySelector } from '../databases/mongo.types'
 
 type Obj = Record<string, any>
 
-export type QueryBuilder<Query, Keys extends Paths<Query> = Paths<Query>> = {
+export type QueryBuilder<
+    Query,
+    Keys extends PathsWithDotNotation<Query> = PathsWithDotNotation<Query>,
+> = {
     [K in Keys]?: QuerySelector<TypeAtPath<Query, K>> | TypeAtPath<Query, K>
 } & { id?: string | QuerySelector<string> } & RootQuerySelector<Query>
-
-export type ValuesWithPaths<
-    Values,
-    Keys extends Paths<Values> = Paths<Values>,
-> = {
-    [K in RequiredKeys<Values, Keys>]: TypeAtPath<Values, K>
-} & {
-    [K in OptionalKeys<Values, Keys>]?: TypeAtPath<Values, K>
-}
-
-type IsOptional<T, K extends keyof T> = {} extends Pick<T, K> ? true : false
-
-type IsPathOptional<T, P extends string> = P extends `${infer K}.${infer Rest}`
-    ? K extends keyof T
-        ? IsOptional<T, K> extends true
-            ? true
-            : IsPathOptional<T[K], Rest>
-        : true
-    : P extends keyof T
-      ? IsOptional<T, P>
-      : true
-
-type RequiredKeys<Values, Keys extends Paths<Values>> = {
-    [K in Keys]: IsPathOptional<Values, K> extends true ? never : K
-}[Keys]
-
-type OptionalKeys<Values, Keys extends Paths<Values>> = Exclude<
-    Keys,
-    RequiredKeys<Values, Keys>
->
-
-type Paths<T, D extends number = 3> = [D] extends [never]
-    ? never
-    : T extends object
-      ? {
-            [K in keyof T]-?: K extends string | number
-                ? `${K}` | Join<K, Paths<T[K], Prev[D]>>
-                : never
-        }[keyof T]
-      : ''
 
 type TypeAtPath<T, P extends string> = P extends `${infer K}.${infer Rest}`
     ? K extends keyof T
@@ -71,35 +35,3 @@ export type PathKeys<Query extends Obj, Prefix extends string = ''> = {
         ? PathKeys<Query[K], `${K}.`>
         : Query[K]
 }
-
-type Join<K, P> = K extends string | number
-    ? P extends string | number
-        ? `${K}${'' extends P ? '' : '.'}${P}`
-        : never
-    : never
-
-type Prev = [
-    never,
-    0,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13,
-    14,
-    15,
-    16,
-    17,
-    18,
-    19,
-    20,
-    ...0[],
-]
